3cc76546814478ffad1df22d94ff1164dafb2cbc
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-14 13:49:43 -0700

    Added check for repeat comparisons to prevent invalid comparisons from entering the database

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,11 +1,11 @@
 class CompareController < ApplicationController
   def index
-  	#get the list of frosh for processing
-	@froshes = Frosh.all
-	#now we need to start our sorting process
-	catch :notcompared do
-	  @sorted = merge_sort(@froshes);
-	end
+    #get the list of frosh for processing
+    @froshes = Frosh.all
+    #now we need to start our sorting process
+    catch :notcompared do
+      @sorted = merge_sort(@froshes);
+    end
   end
 
   def merge_sort(a)
@@ -15,43 +15,52 @@ class CompareController < ApplicationController
   end
 
   def split_array a
-	mid = (a.size / 2).round
-	[a.take(mid), a.drop(mid)]
+    mid = (a.size / 2).round
+    [a.take(mid), a.drop(mid)]
   end
 
   def combine a, b
     return b.empty? ? a : b if a.empty? || b.empty?
-	#find out which is better
-	#is a better?
-    if Comparison.where(:winner=>a.first.id).where(:loser=>b.first.id).count != 0
-	  smallest = a.shift
-	elsif Comparison.where(:winner=>b.first.id).where(:loser=>a.first.id).count != 0
-	  smallest = b.shift
-	else
-	  #they have not been compared, we must compare them
-	  redirect_to action: "compare", left:a.first.id, right:b.first.id
-	  throw :notcompared
-	end
-	#smallest = a.first <= b.first ? a.shift : b.shift
+    #find out which is better
+    #is a better?
+    if Comparison.where(:winner=>a.first.id).
+                    where(:loser=>b.first.id).count != 0
+      smallest = a.shift
+    elsif Comparison.where(:winner=>b.first.id).
+                    where(:loser=>a.first.id).count != 0
+      smallest = b.shift
+    else
+      #they have not been compared, we must compare them
+      redirect_to action: "compare", left:a.first.id, right:b.first.id
+      throw :notcompared
+    end
+    #smallest = a.first <= b.first ? a.shift : b.shift
     combine(a, b).unshift(smallest)
   end
 
   def compare
-  	@leftfrosh = Frosh.find(params[:left])
-	@rightfrosh = Frosh.find(params[:right])
-	#TODO render frosh data
+    @leftfrosh = Frosh.find(params[:left])
+    @rightfrosh = Frosh.find(params[:right])
+    #TODO render frosh data
   end
 
   def savecmp
-  	@loserfrosh = Frosh.find(params[:loser])
-	@winnerfrosh = Frosh.find(params[:winner])
-	@comparison = Comparison.new
-	@comparison.winner = @winnerfrosh.id
-	@comparison.loser = @loserfrosh.id
-
-	@comparison.save
-
-	
-  	redirect_to action: "index"
+    winnerid = params[:winner]
+    loserid = params[:loser]
+    if Comparison.where(:winner=>winnerid).
+                      where(:loser=>loserid).count == 0  &&
+              Comparison.where(:winner=>loserid).
+                      where(:loser=>winnerid).count == 0
+      @comparison = Comparison.new
+      @comparison.winner = winnerid
+      @comparison.loser = loserid
+  
+      @comparison.save
+  
+      
+      redirect_to action: "index"
+    else
+      render text: "plz don't do that"
+    end
   end
 end
