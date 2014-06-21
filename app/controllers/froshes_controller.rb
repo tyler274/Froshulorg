ed4590cb3aa651878ad524b60f21a59ec1058e25
Andrew Zhao <azhao@dmail.caltech.edu>
2014-06-21 16:34:20 -0700

    Added edit functionality to frosh page

--- app/controllers/froshes_controller.rb
+++ app/controllers/froshes_controller.rb
@@ -11,7 +11,20 @@ class FroshesController < ApplicationController
 	redirect_to froshes_path
   end
 
+  def update
+  	@frosh = Frosh.find(params[:id])
+	@frosh.name = params[:frosh][:name]
+	@frosh.dinner = params[:frosh][:dinner]
+	@frosh.dessert = params[:frosh][:dessert]
+	@frosh.save
+	redirect_to froshes_path
+  end
+
   def show
+  	@frosh = Frosh.find(params[:id])
+  end
+
+  def edit
   	@frosh = Frosh.find(params[:id])
   end
 
